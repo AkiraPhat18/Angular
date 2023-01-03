f90b1911b89c1b369df547e22135d1a314f05a5e
AkiraPhat18 <phatnguyen19111997@gmail.com>
2023-01-03 11:20:01 +0700

    data binding

--- Angular2021/polyshopangular/src/app/app.module.ts
+++ Angular2021/polyshopangular/src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { ProductDetailComponent } from './componets/product-detail/product-detail.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './componets/product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
