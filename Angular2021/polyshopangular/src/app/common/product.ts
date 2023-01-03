3c442cd16a6f1bba00b046ea663003bf693e1b21
AkiraPhat18 <phatnguyen19111997@gmail.com>
2023-01-03 09:05:50 +0700

    new folder

---
+++ Angular2021/polyshopangular/src/app/common/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  productCode: String;
+  productName: String
+
+  constructor(productCode: String,productName: String){
+    this.productCode = productCode,
+    this.productName = productName
+  }
+}
---
+++ Angular2021/polyshopangular/src/app/componets/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+//định nghĩa lớp
+export class ProductListComponent implements OnInit {
+  products : Product[] = [
+    new Product('P01','SmartPhone'),
+    new Product('P02','Laptop')
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
