5dbe334695a8a62048259151e0188c96d6110b24
AkiraPhat18 <phatnguyen19111997@gmail.com>
2023-01-09 10:55:15 +0700

    vd về ngFor angular

--- Angular2021/hello-angular/src/app/app.component.ts
+++ Angular2021/hello-angular/src/app/app.component.ts
@@ -13,7 +13,19 @@ export class AppComponent {
     quantity: 200,
     price: 500
   }
+
+public persons =  [
+    {name_employee: 'Phát', position: 'Junior BA', salary: 1000},
+    {name_employee: 'Linh', position: 'PM', salary: 2000},
+    {name_employee: 'Tuấn', position: 'Dev', salary: 1000},
+    {name_employee: 'Hà', position: 'Tester', salary: 4000}
+];
+
+  constructor() {}
+
+  public ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log('Nhân viên =',this.persons);
+  }
 }
-
-
-
